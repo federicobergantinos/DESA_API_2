3ccfb1725723de48158b2d066fff9bc7a04aa459
federico.bergantinos@kueski.com <federico.bergantinos@kueski.com>
2024-05-21 23:28:02 -0300

    Fix

--- backend/configurations/database/sequelizeConnection.js
+++ backend/configurations/database/sequelizeConnection.js
@@ -1,21 +1,45 @@
 const { Sequelize } = require('sequelize')
+const createLogger = require('../Logger')
+const logger = createLogger(__filename)
 
 let sequelize
 
-sequelize = new Sequelize({
-  database: process.env.POSTGRES_DB,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  dialect: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-})
+try {
+  sequelize = new Sequelize({
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialect: 'postgres',
+    logging: false,
+    // Elimina las opciones de SSL
+    dialectOptions:
+      process.env.NODE_ENV === 'production'
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+  })
+
+  // Prueba de conexión
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.')
+    })
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err)
+    })
+} catch (error) {
+  logger.error('Error creating Sequelize instance', {
+    message: error.message,
+    stack: error.stack,
+  })
+  throw new Error('Error creating Sequelize instance')
+}
 
 module.exports = sequelize
