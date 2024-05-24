299aefad0e75706ef77739bc9cd89914e3d7f239
jmutolo <joaquinmutolo@gmail.com>
2024-05-23 21:50:51 -0300

    missions

--- frontend/screens/Missions.js
+++ frontend/screens/Missions.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {
   View,
   ImageBackground,
@@ -9,9 +9,13 @@ import {
 } from 'react-native'
 import { theme, Block } from 'galio-framework'
 import { Images, walletTheme } from '../constants'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 const { width, height } = Dimensions.get('screen')
 
 const Buy = () => {
+
+  const [totalXWC, setTotalXWC] = useState(400);
+
   // Creating separate cards for each detail
   const Card = ({ title, children }) => (
     <View style={styles.detailCard}>
@@ -19,6 +23,128 @@ const Buy = () => {
       {children}
     </View>
   )
+
+  const NewCardButton = ({title, children}) => {
+    return(
+      <View style={styles.newCardCentered}>
+        {children}
+      </View>
+    )
+  }
+
+  const MiddleText = ({children}) => (
+    <View style={styles.descriptionWrapper}>
+      <Text style={styles.description}>{children}</Text>
+    </View>
+  )
+
+  const ExchangeCard = ({xwc, xcoin}) => (
+    <View style={styles.exchangeCardWrapper}>
+      <Text style={styles.exchangeCardText}>{xwc + " XWC"}</Text>
+      <Text style={styles.exchangeCardText}>{"="}</Text>
+      <Text style={styles.exchangeCardText}>{xcoin + " Xcoin"}</Text>
+      
+
+    </View>
+  )
+
+
+ 
+  const missionSchema = [{
+    key: 0,
+    title: 'Misión 1',
+    description: 'Descripción de la misión 1',
+    reward: 100,
+    claimed: false,
+  },
+  {
+    key: 1,
+    title: 'Misión 2',
+    description: 'Descripción de la misión 2',
+    reward: 100,
+    claimed: true,
+  },
+  {
+    key: 2,
+    title: 'Misión 3',
+    description: 'Descripción de la misión 3',
+    reward: 300,
+    claimed: true,
+  },
+  {
+    key: 3,
+    title: 'Misión 4',
+    description: 'Descripción de la misión 4',
+    reward: 400,
+    claimed: false, 
+  },
+  {
+    key: 4,
+    title: 'Misión 5',
+    description: 'Descripción de la misión 5',
+    reward: 400,
+    claimed: false, 
+  },
+  {
+    key: 5,
+    title: 'Misión 6',
+    description: 'Descripción de la misión 5',
+    reward: 400,
+    claimed: false, 
+  }
+  ]
+
+  const [stateMissionSchema, setStateMissionSchema] = useState(missionSchema);
+
+  const handleClaim = (index) => {
+
+    const updatedMissionSchema = [...stateMissionSchema];
+    updatedMissionSchema[index].claimed = true;
+    setTotalXWC(totalXWC + updatedMissionSchema[index].reward);
+    setStateMissionSchema(updatedMissionSchema);
+    /*
+    missionSchema[index].claimed = true;
+    setTotalXWC(totalXWC + missionSchema[index].reward);
+    
+    setStateMissionSchema(missionSchema);
+    */
+  }
+
+  const CompletedMissions = ({data}) => (
+    <View style={styles.missionsWrapper}>
+      <Text style={styles.missionsTitle}>Misiones Completadas</Text>
+      
+        {data.map((mission, index) => (
+          <View style={styles.missionRowWrapper} key={index}>
+          <View style={styles.missionsRowColumn} >
+            <Text style={styles.missionsRowColumnTitle}>{mission.title}</Text>
+            <Text style={styles.missionsRowColumnDescription}>{mission.description}</Text>
+          </View>
+          {!mission.claimed ? <TouchableOpacity style={styles.claimButton} onPress={() => {handleClaim(mission.key)}}><Text style={styles.claimButtonText}>Claim</Text></TouchableOpacity> : <View style={styles.alreadyClaimedIcon}><Text style={styles.alreadyClaimedIconText}>{"+" + mission.reward}</Text></View>}
+          </View>
+        ))}
+      
+
+    </View>
+  )
+
+  const MissionsToComplete = ({data}) => (
+    <View style={styles.missionsWrapper}>
+      <Text style={styles.missionsTitle}>Misiones a completar</Text>
+        {
+          data.map((mission, index) => (
+            <View style={styles.missionRowWrapper} key={index}>
+              <View style={styles.misisonsRowColumn}>
+                <Text style={styles.missionsRowColumnTitle}>{mission.title}</Text>
+                 <Text style={styles.missionsRowColumnDescription}>{mission.description}</Text>
+              </View>
+            </View>
+          ))
+        }
+    </View>
+  )
+
+  
 
   return (
     <Block flex style={styles.home}>
@@ -31,16 +157,23 @@ const Buy = () => {
           {
             <>
               {/* Buy1 */}
-              <Card title="Buy1">
-                <Text style={styles.title}>Buy1</Text>
-                <Text style={styles.description}>Buy1</Text>
+              <Card title="XWC Totales" style={styles.cardBackgroundColor}>
+                <Text style={styles.title}>{ totalXWC + " XWC"}</Text>
               </Card>
 
               {/* Buy2 */}
-              <Card title="Buy2">
-                <Text style={styles.title}>Buy2</Text>
-                <Text style={styles.description}>Buy2</Text>
-              </Card>
+              <NewCardButton>
+                <Text style={styles.newCardText}>Misiones</Text>
+              </NewCardButton>
+
+              <MiddleText>¡Obtené más XWC completando las diferentes misiones y desafios!</MiddleText>
+
+              <CompletedMissions data={stateMissionSchema} />
+              <View style={styles.bottomMargin}>
+              <MissionsToComplete data={missionSchema} />
+              </View>
+              
+              
             </>
           }
         </ScrollView>
@@ -59,7 +192,7 @@ const styles = StyleSheet.create({
     marginTop: -100,
   },
   detailCard: {
-    backgroundColor: theme.COLORS.WHITE,
+    backgroundColor: walletTheme.COLORS.VIOLET,
     padding: theme.SIZES.BASE,
     marginHorizontal: theme.SIZES.BASE,
     marginTop: theme.SIZES.BASE,
@@ -71,12 +204,14 @@ const styles = StyleSheet.create({
     elevation: 2,
   },
   cardTitle: {
-    fontSize: 16,
+    fontSize: 22,
     marginBottom: theme.SIZES.BASE / 2,
+    color: '#FFFFFF'
   },
   title: {
-    fontSize: 18,
+    fontSize: 34,
     fontWeight: 'bold',
+    color: theme.COLORS.WHITE,
   },
   description: {
     fontSize: 14,
@@ -86,6 +221,157 @@ const styles = StyleSheet.create({
   scrollViewContent: {
     paddingTop: 100,
   },
+  cardBackgroundColor: {
+    backgroundColor: '#000000',
+  },
+  centerCard: {
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  newCardCentered: {
+    backgroundColor: walletTheme.COLORS.VIOLET,
+    padding: theme.SIZES.BASE,
+    marginHorizontal: theme.SIZES.BASE,
+    marginTop: theme.SIZES.BASE,
+    borderRadius: theme.SIZES.BASE / 2,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.1,
+    elevation: 2,
+    display: 'flex',
+    alignItems: 'center',
+    color: '#FFFFFF',
+  },
+  newCardText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  descriptionWrapper: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingLeft: 50,
+    paddingRight: 50,
+  },
+  description: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+  exchangeCardWrapper: {
+    backgroundColor: theme.COLORS.WHITE,
+    padding: theme.SIZES.BASE,
+    marginHorizontal: theme.SIZES.BASE,
+    marginTop: theme.SIZES.BASE,
+    borderRadius: theme.SIZES.BASE / 2,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.1,
+    elevation: 2,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+    color: '#5144A6',
+  },
+  exchangeCardsColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 10,
+    paddingTop: 30,
+  },
+  exchangeCardText: {
+    fontSize: 30,
+    color: '#5144A6',
+    fontWeight: 'bold',
+  },
+  missionsWrapper: {
+    backgroundColor: theme.COLORS.WHITE,
+    padding: theme.SIZES.BASE,
+    marginHorizontal: theme.SIZES.BASE,
+    marginTop: theme.SIZES.BASE,
+    borderRadius: theme.SIZES.BASE / 2,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.1,
+    elevation: 2,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 10,
+    marginBottom: 20,
+  },
+  missionsTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#5144A6',
+  },
+  missionRowWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  missionsRowColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 10,
+  },
+  missionsRowColumnTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#545F71',
+  },
+  missionsRowColumnDescription: {
+    fontSize: 16,
+    color: '#545F71',
+    maxWidth: 90,
+    textWrap: 'wrap',
+  },
+  claimButton: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#5144A6',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    height: 56,
+    width: 91,
+    borderRadius: 10,
+  },
+  claimButtonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  alreadyClaimedIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#EEEBFF',
+    color: '#545F71',
+    fontWeight: 'bold',
+    height: 56,
+    width: 91,
+    borderRadius: 10,
+  },
+  alreadyClaimedIconText: {
+    color: '#545F71',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  bottomMargin: {
+    marginBottom: 200,
+  },
+
+
 })
 
 export default Buy
--- frontend/screens/MissionsStore.js
+++ frontend/screens/MissionsStore.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {
   View,
   ImageBackground,
@@ -20,6 +20,31 @@ const Buy = () => {
     </View>
   )
 
+  const NewCardButton = ({title, children}) => {
+    return(
+      <View style={styles.newCardCentered}>
+        {children}
+      </View>
+    )
+  }
+
+  const MiddleText = ({children}) => (
+    <View style={styles.descriptionWrapper}>
+      <Text style={styles.description}>{children}</Text>
+    </View>
+  )
+
+  const ExchangeCard = ({xwc, xcoin}) => (
+    <View style={styles.exchangeCardWrapper}>
+      <Text style={styles.exchangeCardText}>{xwc + " XWC"}</Text>
+      <Text style={styles.exchangeCardText}>{"="}</Text>
+      <Text style={styles.exchangeCardText}>{xcoin + " Xcoin"}</Text>
+      
+
+    </View>
+  )
+  
+
   return (
     <Block flex style={styles.home}>
       <ImageBackground source={Images.Background} style={styles.background}>
@@ -31,16 +56,32 @@ const Buy = () => {
           {
             <>
               {/* Buy1 */}
-              <Card title="Buy1">
-                <Text style={styles.title}>Buy1</Text>
-                <Text style={styles.description}>Buy1</Text>
+              <Card title="XWC Totales" style={styles.cardBackgroundColor}>
+                <Text style={styles.title}>400 XWC</Text>
               </Card>
 
               {/* Buy2 */}
-              <Card title="Buy2">
-                <Text style={styles.title}>Buy2</Text>
-                <Text style={styles.description}>Buy2</Text>
-              </Card>
+              <NewCardButton>
+                <Text style={styles.newCardText}>Canjear</Text>
+              </NewCardButton>
+
+              <MiddleText>Canjea tus XWC por Xcoin para tener en tu cuenta y acceder a beneficios increibles</MiddleText>
+
+              <NewCardButton>
+                <Text style={styles.newCardText}>Opciones</Text>
+              </NewCardButton>
+
+              <View style={styles.exchangeCardsColumn}>
+                <ExchangeCard xwc="20000" xcoin="2"/>
+                <ExchangeCard xwc="40000" xcoin="4"/>
+                <ExchangeCard xwc="60000" xcoin="6"/>
+                <ExchangeCard xwc="80000" xcoin="8"/>
+                <ExchangeCard xwc="100000" xcoin="10"/>
+                <ExchangeCard xwc="120000" xcoin="12"/>
+                <ExchangeCard xwc="140000" xcoin="14"/>
+              </View>
+              
+              
             </>
           }
         </ScrollView>
@@ -59,7 +100,7 @@ const styles = StyleSheet.create({
     marginTop: -100,
   },
   detailCard: {
-    backgroundColor: theme.COLORS.WHITE,
+    backgroundColor: walletTheme.COLORS.VIOLET,
     padding: theme.SIZES.BASE,
     marginHorizontal: theme.SIZES.BASE,
     marginTop: theme.SIZES.BASE,
@@ -71,12 +112,14 @@ const styles = StyleSheet.create({
     elevation: 2,
   },
   cardTitle: {
-    fontSize: 16,
+    fontSize: 22,
     marginBottom: theme.SIZES.BASE / 2,
+    color: '#FFFFFF'
   },
   title: {
-    fontSize: 18,
+    fontSize: 34,
     fontWeight: 'bold',
+    color: theme.COLORS.WHITE,
   },
   description: {
     fontSize: 14,
@@ -86,6 +129,78 @@ const styles = StyleSheet.create({
   scrollViewContent: {
     paddingTop: 100,
   },
+  cardBackgroundColor: {
+    backgroundColor: '#000000',
+  },
+  centerCard: {
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  newCardCentered: {
+    backgroundColor: walletTheme.COLORS.VIOLET,
+    padding: theme.SIZES.BASE,
+    marginHorizontal: theme.SIZES.BASE,
+    marginTop: theme.SIZES.BASE,
+    borderRadius: theme.SIZES.BASE / 2,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.1,
+    elevation: 2,
+    display: 'flex',
+    alignItems: 'center',
+    color: '#FFFFFF',
+  },
+  newCardText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  descriptionWrapper: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingLeft: 50,
+    paddingRight: 50,
+  },
+  description: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+  exchangeCardWrapper: {
+    backgroundColor: theme.COLORS.WHITE,
+    padding: theme.SIZES.BASE,
+    marginHorizontal: theme.SIZES.BASE,
+    marginTop: theme.SIZES.BASE,
+    borderRadius: theme.SIZES.BASE / 2,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.1,
+    elevation: 2,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+    color: '#5144A6',
+  },
+  exchangeCardsColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 10,
+    paddingTop: 30,
+    marginBottom: 200,
+  },
+  exchangeCardText: {
+    fontSize: 30,
+    color: '#5144A6',
+    fontWeight: 'bold',
+  }
 })
 
 export default Buy
