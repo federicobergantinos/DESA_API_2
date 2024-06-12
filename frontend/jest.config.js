9737e56cce22033407b617747be6187aead9ba8d
federico.bergantinos@kueski.com <federico.bergantinos@kueski.com>
2024-06-12 16:18:16 -0300

    Estructura de pruebas unitarias

---
+++ frontend/jest.config.js
@@ -0,0 +1,11 @@
+// backend/jest.config.js
+module.exports = {
+  coverageDirectory: 'coverage/unit',
+  testMatch: ['**/tests/unit/**/*.test.js'],
+}
+
+// frontend/jest.config.js
+module.exports = {
+  coverageDirectory: 'coverage/unit',
+  testMatch: ['**/tests/unit/**/*.test.js'],
+}
