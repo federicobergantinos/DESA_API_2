c43ee3f1f07196cb4ac7c4d17cff62592e4da852
federico.bergantinos@kueski.com <federico.bergantinos@kueski.com>
2024-06-17 00:36:56 -0300

    Unit tests

--- backend/services/exchangeRateService.js
+++ backend/services/exchangeRateService.js
@@ -1,32 +1,38 @@
-const ExchangeRate = require('../entities/exchangeRate');
-const NotFound = require('../Errors/NotFound');
+const ExchangeRate = require('../entities/exchangeRate')
+const NotFound = require('../Errors/NotFound')
 
 const getExchangeRate = async (currency) => {
-  const exchangeRate = await ExchangeRate.findOne({ where: { currency } });
+  const exchangeRate = await ExchangeRate.findOne({ where: { currency } })
   if (!exchangeRate) {
-    throw new NotFound('Exchange rate not found');
+    throw new NotFound('Exchange rate not found')
   }
-  return exchangeRate;
-};
+  return exchangeRate
+}
 
 const getAllExchangeRates = async () => {
-  return await ExchangeRate.findAll();
-};
+  return await ExchangeRate.findAll()
+}
 
 const getExchangeRateForCurrency = async (currency) => {
   if (currency === 'USD' || currency === 'XCN') {
-    return await getExchangeRate('XCN');
+    const rateXCN = await getExchangeRate('XCN')
+    return {
+      XCN: rateXCN.rate,
+    }
   } else if (currency === 'ARS') {
-    const rateToARS = await getExchangeRate('ARS');
-    const rateToXCN = await getExchangeRate('XCN');
-    return { rate: rateToARS.rate * rateToXCN.rate };
+    const rateARS = await getExchangeRate('ARS')
+    const rateXCN = await getExchangeRate('XCN')
+    return {
+      ARS: rateARS.rate,
+      XCN: rateXCN.rate,
+    }
   } else {
-    throw new NotFound('Unsupported currency');
+    throw new NotFound('Unsupported currency')
   }
-};
+}
 
 module.exports = {
   getExchangeRate,
   getAllExchangeRates,
   getExchangeRateForCurrency,
-};
+}
