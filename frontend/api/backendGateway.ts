2710263488dad8e993c55dbb3990456dfc93cc0d
federico.bergantinos@kueski.com <federico.bergantinos@kueski.com>
2024-06-20 16:39:47 -0300

    [PRODU-27800] Init Fraud Service

--- frontend/api/backendGateway.ts
+++ frontend/api/backendGateway.ts
@@ -6,7 +6,7 @@ import { ContactDTO, ContactsDTO } from './ContactDTO';
 import { AccountDTO, AccountSummaryDTO } from './AccountDTO';
 
 // const api = axios.create({ baseURL: "https://www.wallet-elb.federicobergantinos.com/" });
-const api = axios.create({ baseURL: 'http://192.168.1.110:8080' });
+const api = axios.create({ baseURL: 'http://192.168.1.112:8080' });
 // const api = axios.create({baseURL: 'https://277c-170-150-153-225.ngrok-free.app:8080'});
 
 const transactionBaseUrl = '/v1/transactions';
